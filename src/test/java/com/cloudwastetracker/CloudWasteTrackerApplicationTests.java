9e8de2690ca7d98dc016b56e88f36bb724f6d1f5
Ian Willis <ianwillis98@gmail.com>
2020-01-26 02:15:37 -0500

    Project setup

---
+++ src/test/java/com/cloudwastetracker/CloudWasteTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudwastetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudWasteTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
