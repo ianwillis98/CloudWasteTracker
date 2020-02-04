614b7d186d2c07d6987ecc2844b73d47e001340e
Ian Willis <ianwillis98@gmail.com>
2020-02-04 18:29:39 -0500

    beginnings of rightsizing call

--- src/main/java/com/cloudwastetracker/CloudWasteTracker/vendor/VendorsModel.java
+++ src/main/java/com/cloudwastetracker/CloudWasteTracker/vendor/VendorsModel.java
@@ -6,46 +6,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class VendorsModel {
 
-	private List<VendorModel> result;
-
-	public VendorsModel() {
-
-	}
-
-	public VendorsModel(List<VendorModel> result) {
-		this.result = result;
-	}
-
-	public List<VendorModel> getResult() {
-		return result;
-	}
+	public List<VendorModel> result;
 
 	public static class VendorModel {
 
-		private String key;
-		private String label;
-		private String description;
+		public String key;
+		public String label;
+		public String description;
 
-		public VendorModel() {
-
-		}
-
-		public VendorModel(String key, String label, String description) {
-			this.key = key;
-			this.label = label;
-			this.description = description;
-		}
-
-		public String getKey() {
-			return key;
-		}
-
-		public String getLabel() {
-			return label;
-		}
-
-		public String getDescription() {
-			return description;
-		}
 	}
 }
