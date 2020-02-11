c5aaae5534eaf5d826ab4e0e46a0f90206a02291
Timothy Regrut <timothyregrut@Timothys-MacBook.local>
2020-02-11 17:40:36 -0500

    Automated Data Collection for Reports

---
+++ src/main/java/com/cloudwastetracker/CloudWasteTracker/Record/SaveDataToDB.java
@@ -0,0 +1,41 @@
+package com.cloudwastetracker.CloudWasteTracker.Record;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import com.cloudwastetracker.CloudWasteTracker.cloudability.CloudabilityClient;
+import com.cloudwastetracker.CloudWasteTracker.rightsizing.RightsizingModel;
+
+@Component
+public class SaveDataToDB {
+	@Autowired
+	ReportsRepository temp;
+
+	@Autowired
+	CloudabilityClient c;
+	
+	
+	public void SaveData(){
+		
+		//https://www.baeldung.com/spring-data-jpa-pagination-sorting for sorting by timestamp
+		
+		Reports repo = new Reports();
+		RightsizingModel model = c.fetchRightsizing("i-003f7b7543aae4f52").getBody();
+		repo.setMoney_spent(model.result.get(0).totalSpend);
+		repo.setTime_stamp(new Timestamp(new Date().getTime()));
+		repo.setMoney_wasted(model.result.get(0).recommendations.get(0).savings);
+		repo.setServer_type(model.result.get(0).nodeType);
+		repo.setLocal_capacity(model.result.get(0).localCapacity);
+		repo.setLocal_drives(model.result.get(0).localDrives);
+		repo.setCpu_capacity(model.result.get(0).cpuCapacity);
+		repo.setMemory_capacity(model.result.get(0).memoryCapacity);
+		temp.save(repo);
+		
+	}
+
+}
