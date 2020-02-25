fdf995b97e73aca592847470c23d3a81a5f646f9
KCoffey64 <54513338+KCoffey64@users.noreply.github.com>
2020-02-25 18:20:15 -0500

    Update VendorsTest.java
    Added testGetVendorID, testSetVendorLabel, testSetVendorKey

--- src/test/java/com/cloudwastetracker/CloudWasteTracker/VendorsTest.java
+++ src/test/java/com/cloudwastetracker/CloudWasteTracker/VendorsTest.java
@@ -17,17 +17,6 @@ public class VendorsTest {
     VendorsRepository subject;
 
     @Test
-    public void testRepositorySave() throws Exception {
-        Vendors v = new Vendors();
-        String key = "testkey";
-        v.setVendorKey(key);
-        v.setVendorDescription("testdescription");
-        v.setVendorLabel("testlabel");
-        subject.save(v);
-        assertTrue(subject.findById(key).isPresent());
-    }
-
-    @Test
     public void testRepositoryGet() throws Exception {
         Vendors v = new Vendors();
         String key = "testkey";
@@ -53,5 +42,52 @@ public class VendorsTest {
         subject.save(v);
 
         assertTrue(subject.findAll().iterator().hasNext());
+    }
+    
+    @Test
+    public void testSetVendorKey() throws Exception {
+    	Vendors v = new Vendors();
+    	String key = "testkey";
+    	v.setVendorKey(key);
+    	assertEquals(key, v.getVendorKey());
+    }
+    
+    @Test
+    public void testSetVendorLabel() throws Exception {
+    	Vendors v = new Vendors();
+    	String l = "testlabel";
+    	v.setVendorLabel(l);
+    	assertEquals(l, v.getVendorLabel());
+    }
+
+    @Test
+    public void testRepositorySave() throws Exception {
+        Vendors v = new Vendors();
+        String key = "testkey";
+        String label = "testlabel";
+        String description = "testdescription";
+        v.setVendorKey(key);
+        v.setVendorLabel(label);
+        v.setVendorDescription(description);
+        subject.save(v);
+        assertTrue(subject.findById(key).isPresent());
+    }
+
+    @Test
+    public void testGetVendorID() throws Exception {
+        Vendors v = new Vendors();
+        String key = "testkey";
+        String label = "testlabel";
+        String description = "testdescription";
+        v.setVendorKey(key);
+        v.setVendorLabel(label);
+        v.setVendorDescription(description);
+        subject.save(v);
+
+        assertTrue(subject.findById(key).isPresent());
+        Vendors v2 = subject.findById(key).get();
+        assertEquals(key, v2.getVendorKey());
+        assertEquals(label, v2.getVendorLabel());
+        assertEquals(description, v2.getVendorDescription());
     }
 }
