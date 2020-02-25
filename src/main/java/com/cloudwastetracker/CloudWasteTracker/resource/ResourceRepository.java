d40a46625e27603985897a28c1df4f6013c12dd0
Timothy Regrut <timothyregrut@Timothys-MacBook.local>
2020-02-25 17:42:38 -0500

    added query to resource table

--- src/main/java/com/cloudwastetracker/CloudWasteTracker/resource/ResourceRepository.java
+++ src/main/java/com/cloudwastetracker/CloudWasteTracker/resource/ResourceRepository.java
@@ -3,5 +3,5 @@ package com.cloudwastetracker.CloudWasteTracker.resource;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ResourceRepository extends CrudRepository<Resource, String> {
-
+	Long countByResourceId(String resourceID);
 }
