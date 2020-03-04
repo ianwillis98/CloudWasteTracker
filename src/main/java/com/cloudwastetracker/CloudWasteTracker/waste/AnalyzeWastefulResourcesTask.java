944baf60f19a02dc37576bdde13fd0be0e7fed70
Timothy Regrut <timothyregrut@Timothys-MacBook.local>
2020-03-04 15:08:20 -0500

    commit

--- src/main/java/com/cloudwastetracker/CloudWasteTracker/waste/AnalyzeWastefulResourcesTask.java
+++ src/main/java/com/cloudwastetracker/CloudWasteTracker/waste/AnalyzeWastefulResourcesTask.java
@@ -79,5 +79,6 @@ public class AnalyzeWastefulResourcesTask {
 
             resourceWasteRepository.save(waste);
         }
+        
     }
 }
--- src/main/java/com/cloudwastetracker/CloudWasteTracker/waste/ResourceWasteRepository.java
+++ src/main/java/com/cloudwastetracker/CloudWasteTracker/waste/ResourceWasteRepository.java
@@ -1,7 +1,11 @@
 package com.cloudwastetracker.CloudWasteTracker.waste;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
-public interface ResourceWasteRepository extends CrudRepository<ResourceWaste, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+public interface ResourceWasteRepository extends JpaRepository<ResourceWaste, Long> {
+	@Query(value = "Select * from resource_waste where resource_id = ?1 order by created_at ASC", nativeQuery = true)
+	public List<ResourceWaste> findByResourceId(String rid);
 }
