040335947bc03eaab34cbd3534471aad4e74cfc8
Ian Willis <ianwillis98@gmail.com>
2020-03-04 11:16:28 -0500

    web pages

--- src/main/java/com/cloudwastetracker/CloudWasteTracker/waste/ResourceWasteRepository.java
+++ src/main/java/com/cloudwastetracker/CloudWasteTracker/waste/ResourceWasteRepository.java
@@ -1,7 +1,8 @@
 package com.cloudwastetracker.CloudWasteTracker.waste;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ResourceWasteRepository extends CrudRepository<ResourceWaste, Long> {
+public interface ResourceWasteRepository extends JpaRepository<ResourceWaste, Long> {
 
 }
