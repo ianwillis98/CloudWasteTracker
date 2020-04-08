dd43e2a0b8b89704a576754eca398daff120254e
Timothy Regrut <timothyregrut@Timothys-MacBook.local>
2020-04-08 17:11:21 -0400

    added ability to get data by application and by department

--- src/main/java/com/cloudwastetracker/CloudWasteTracker/waste/ResourceWasteRepository.java
+++ src/main/java/com/cloudwastetracker/CloudWasteTracker/waste/ResourceWasteRepository.java
@@ -17,4 +17,12 @@ public interface ResourceWasteRepository extends JpaRepository<ResourceWaste, Lo
 
     @Query(value = "SELECT * FROM resource_waste WHERE resource_id = :id AND created_at BETWEEN :startDate AND :endDate order by created_at ASC", nativeQuery = true)
     public List<ResourceWaste> findByResourceIdAndDate(@Param("id")String resourceID, @Param("startDate")LocalDate start, @Param("endDate")LocalDate end);
+    
+    @Query(value = "select created_at as 'date', sum(total_spend) as 'totalSpent', sum(recommendation_savings) as 'totalWaste' from resource_waste where resource_id in "
+    		+ "(SELECT resource_id FROM resource where account_name = :appName) AND created_at BETWEEN :startDate AND :endDate group by date(created_at) order by created_at ASC;", nativeQuery = true)
+    public List<WasteData> findByAppName(@Param("appName") String appName, @Param("startDate")LocalDate start, @Param("endDate")LocalDate end);
+    
+    @Query(value = "select created_at as 'date', sum(total_spend) as 'totalSpent', sum(recommendation_savings) as 'totalWaste' from resource_waste where resource_id in "
+    		+ "(SELECT resource_id FROM resource where department = :department) AND created_at BETWEEN :startDate AND :endDate group by date(created_at) order by created_at ASC;", nativeQuery = true)
+    public List<WasteData> findByDepartment(@Param("department") String appName, @Param("startDate")LocalDate start, @Param("endDate")LocalDate end);
 }
